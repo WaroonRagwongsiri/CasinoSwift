51da76ab4158c4bee442ac2e9c9563530e2153b0
admin <st10682@kjn.ac.th>
2024-06-15 21:16:26 -0700

    first

---
+++ CasinoSwift/model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  CasinoSwift
+//
+//  Created by admin on 6/15/24.
+//
+
+import Foundation
+
+class User{
+    var balance:Int
+    
+    init(balance: Int) {
+        self.balance = balance
+    }
+    
+    
+}
