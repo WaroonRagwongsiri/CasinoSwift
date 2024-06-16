de1cd9d9a3034cbde56cf0558887050d19145d4b
admin <st10682@kjn.ac.th>
2024-06-15 21:14:03 -0700

    Initial commit

---
+++ CasinoSwift/CasinoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasinoSwiftApp.swift
+//  CasinoSwift
+//
+//  Created by admin on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CasinoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
